5f0341ae240f3cc45b13e8690937f330db9d8045
Pedro Orellana <pedrorellana21@gmail.com>
2026-07-05 22:34:21 -0700

    fix both print_binary and bitpat_search functions on bitpat_search exercise

--- Chapter11/bitpat_search/main.c
+++ Chapter11/bitpat_search/main.c
@@ -22,7 +22,7 @@ void print_binary(unsigned int word, bool show_trailing_zeroes) {
     u_int8_t num_bits = (sizeof(word) * 8); //this will be different depending on which machine the program is running
     u_int8_t counter = 0;
 
-    for(int8_t i = (num_bits - 1); i-- >= 0;) {
+    for(int8_t i = (num_bits - 1); i >= 0; i--) {
         u_int8_t current_bit = (word >> i) & 1;
 
         if(counter  == 4) {
@@ -52,8 +52,10 @@ u_int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n) {
 
     //pattern extraction and saved into an array
     for(int8_t i  = 0; i < n; i ++) {
+        //from LSB to MSB
         u_int8_t current_bit = (pattern >> i) & 1;
-        pattern_bits[i] = current_bit;
+        //save on array as MSB to LSB
+        pattern_bits[(n-1)-i] = current_bit;
     }
 
     //testing
