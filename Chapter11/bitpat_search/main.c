0ab88ec3bc36199d8d37443a32edc7bd7f4fc79c
Pedro Orellana <pedrorellana21@gmail.com>
2026-07-05 23:50:49 -0700

    first (inefficient) working version of bitpat_search exercise

--- Chapter11/bitpat_search/main.c
+++ Chapter11/bitpat_search/main.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 //prototypes
-u_int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n);
+int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n);
 void print_binary(unsigned int word, bool show_trailing_zeroes);
 
 int main() {
@@ -12,7 +12,8 @@ int main() {
     print_binary(30, true);
     print_binary(30, false);
 
-    bitpat_search(30, 30,8);
+    int8_t answer = bitpat_search(30, 30,8);
+    printf("Answer should be 24. Answer: %d", answer);
 
     return 0;
 }
@@ -47,8 +48,10 @@ void print_binary(unsigned int word, bool show_trailing_zeroes) {
     printf("\n");
 }
 
-u_int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n) {
+int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n) {
+    u_int8_t num_bits_source = (sizeof(source) * 8);
     u_int8_t pattern_bits [n];
+    int8_t answer = -1;
 
     //pattern extraction and saved into an array
     for(int8_t i  = 0; i < n; i ++) {
@@ -58,12 +61,28 @@ u_int8_t bitpat_search(unsigned int source, unsigned int pattern, u_int8_t n) {
         pattern_bits[(n-1)-i] = current_bit;
     }
 
-    //testing
-    for(int8_t i = 0; i < n; i ++) {
-        printf("%d", pattern_bits[i]);
-    }
-    printf("\n");
+    for (int8_t i = (num_bits_source - 1); i >= 0; i --) {
 
-    return 0;
+        if(answer != -1) {
+            return answer;
+        }
+
+        u_int8_t current_bit_source = (source >> i) & 1;
+
+        for(int8_t j = 0; j < n; j++) {
+            u_int8_t current_bit_pattern = pattern_bits[j];
+            if(j == 0 && (current_bit_source == current_bit_pattern)){
+                answer = (num_bits_source - i - 1);
+            }
+
+            if (current_bit_source != current_bit_pattern) {
+                answer = -1;
+                break;
+            }
+            current_bit_source = (source >> (i - j - 1)) & 1;
+        }
+    }
+
+    return answer;
 }
 
